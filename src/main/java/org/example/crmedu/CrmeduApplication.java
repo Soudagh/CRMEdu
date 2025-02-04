36a85d2e7660f8e800188370580bb15dd8af50f0
Soudagh <fr.kristina@inbox.ru>
2025-02-04 18:52:20 +0300

    [feat]: created initial project files

---
+++ src/main/java/org/example/crmedu/CrmeduApplication.java
@@ -0,0 +1,13 @@
+package org.example.crmedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmeduApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrmeduApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/crmedu/CrmeduApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crmedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmeduApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
