729dda629ba827e373829767eeac598579ecd53a
Soudagh <fr.kristina@inbox.ru>
2025-02-05 19:09:53 +0300

    [feat]: handle EntityNotFound and EntityExistsException in AdviceController

--- src/main/java/org/example/crmedu/application/controller/AdviceController.java
+++ src/main/java/org/example/crmedu/application/controller/AdviceController.java
@@ -4,10 +4,27 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import org.example.crmedu.application.dto.error.SystemError;
+import org.example.crmedu.domain.exception.EntityExistsException;
+import org.example.crmedu.domain.exception.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class AdviceController {
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<SystemError> handleEntityNotFoundException(EntityNotFoundException e) {
+    var error = getSystemError(e);
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+  }
+
+  @ExceptionHandler(EntityExistsException.class)
+  public ResponseEntity<SystemError> handleEntityExistsException(EntityExistsException e) {
+    var error = getSystemError(e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+  }
 
   private SystemError getSystemError(Exception e) {
     return new SystemError()
